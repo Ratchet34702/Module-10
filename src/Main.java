4dc7e548aed73b73152b39657d8b2bc4e2dcb144
Ratchet34702 <vlad.berislavskiy@gmail.com>
2022-09-30 16:41:13 +0300

    Third task

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> stringList = List.of("Vold", "Mosha", "Arutr", "Keril", "Viktoria", "Okabe", "Igor");
-        System.out.println(task1(stringList));
-        System.out.println(task2(stringList));
+        List<String> nameList = List.of("Vold", "Mosha", "Arutr", "Keril", "Viktoria", "Okabe", "Igor");
+        String[] numberArray = {"1, 2, 0", "4, 5"};
+        System.out.println(task1(nameList));
+        System.out.println(task2(nameList));
+        System.out.println(task3(numberArray));
 
     }
 
@@ -25,6 +29,14 @@ public class Main {
         return stringList.stream()
                 .map(String::toUpperCase)
                 .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+    }
+
+    static List<Integer> task3(String[] stringArray) {
+        return Arrays.stream(stringArray)
+                .flatMap(s -> Stream.of(s.split(", ")))
+                .map(Integer::parseInt)
+                .sorted()
                 .collect(Collectors.toList());
     }
 }
